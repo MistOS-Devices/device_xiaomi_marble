bf886fbd5b79eb78623d17d3243145be724fb157
dhanush281 <dhanushvozh345@gmail.com>
2026-01-05 19:24:15 +0530

    marble:bringup Lunaris-Aosp

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -24,23 +24,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-#AxionAosp Flags
+# Lunaris Official
+LUNARIS_BUILD_TYPE := OFFICIAL
+TARGET_OPTIMIZED_DEXOPT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_DISABLE_EPPE := true
-TARGET_ENABLE_BLUR := true
-TARGET_INCLUDES_LOS_PREBUILTS := true
-
-# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
-AXION_MAINTAINER := Ashwani
-
-# Processor name (use "_" for spaces)
-AXION_PROCESSOR := Snapdragon®_7+_Gen_2_(SM7475)
-
-# Define rear camera specs (multiple sensors supported)
-AXION_CAMERA_REAR_INFO := 64,8,2  # Example: 50MP + 48MP
-
-# Define front camera specs
-AXION_CAMERA_FRONT_INFO := 16  # Example: 42MP
-
-# Flash Strength
-TORCH_STR_SUPPORTED := true
+WITH_BCR := true
+WITH_GMS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
